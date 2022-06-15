dc1f2606775424eb5b07fde77eae7c04446d9d28
jinhlee19 <jinhlee19@gmail.com>
2022-06-15 09:55:52 +0900

    getServerSideProps & getStatic Props, styles, Displaying Events 1

---
+++ NOTE.md
@@ -0,0 +1,18 @@
+# Snippet
+
+```jsx
+// nil
+import Link from 'next/link';
+// nir
+import Router from 'next/router';
+// niur
+import { useRouter } from 'next/router';
+// nih
+import Head from 'next/head';
+// nf
+export default function () {
+	return <div>Enter</div>;
+}
+// nuur
+const router = useRouter();
+```
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,35 @@
 import Layout from '@/components/Layout';
-export default function HomePage() {
+import { API_URL } from '@/config/index';
+
+export default function HomePage({ events }) {
+	console.log(events);
 	return (
 		<div>
 			<Layout>
-				<h1>Home</h1>
-				<p>This is an app to find the latest DJ and other musical events</p>
-				<p>Version: 1.0.0</p>
+				<h1>Upcoming Events</h1>
+				{events.length === 0 && <h3>No Events to Show</h3>}
+				{events.map(evt => (
+					<h3 key={evt.id}>{evt.name}</h3>
+				))}
 			</Layout>
 		</div>
 	);
 }
+export async function getServerSideProps() {
+	const res = await fetch(`${API_URL}/api/events`);
+	const events = await res.json();
+	console.log(events);
+	return {
+		props: { events },
+	};
+}
+
+// export async function getStaticProps() {
+// 	const res = await fetch(`${API_URL}/api/events`);
+// 	const events = await res.json();
+// 	console.log(events);
+// 	return {
+// 		props: { events },
+// 		revalidate: 1,
+// 	};
+// }
