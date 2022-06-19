c23b60ddf9e49d2517d58fa076fea835f9763b87
jinhlee19 <jinhlee19@gmail.com>
2022-06-19 23:00:17 +0900

    Image Upload 2

--- components/ImageUpload.jsx
+++ components/ImageUpload.jsx
@@ -1,3 +1,13 @@
+import { useState } from 'react';
+import { API_URL } from '@/config/index';
+import styles from '@/styles/Form.module.css';
+
 export default function ImageUpload() {
-	return <div>Image Upload Component</div>;
+	const [image, setImage] = useState(null);
+
+	return (
+		<div className="styles.form">
+			<h1>Upload Event Image</h1>
+		</div>
+	);
 }
