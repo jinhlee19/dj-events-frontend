b58ed68a53e4cda6fd2d03c078c2b68e6c4a882f
jinhlee19 <jinhlee19@gmail.com>
2022-06-29 21:58:28 +0900

    Section 6 End - Event Pagination Component

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,13 +1,12 @@
 import Layout from '@/components/Layout';
 import EventItem from '@/components/Eventitem';
 import Link from 'next/link';
-import { API_URL } from '@/config/index';
-
-const PER_PAGE = 4;
+import { API_URL, PER_PAGE } from '@/config/index';
+import Pagination from '@/components/Pagination';
 
 export default function EventsPage({ events, page, total }) {
 	// console.log(events);
-	const lastPage = Math.ceil(total / PER_PAGE);
+
 	return (
 		<div>
 			<Layout>
@@ -16,19 +15,7 @@ export default function EventsPage({ events, page, total }) {
 				{events.map(evt => (
 					<EventItem key={evt.id} evt={evt} />
 				))}
-				{page > 1 && (
-					<Link href={`/events?page=${page - 1}`}>
-						<a className="btn-secondary">Prev</a>
-					</Link>
-				)}
-
-				{page < lastPage && (
-					<Link href={`/events?page=${page + 1}`}>
-						<a className="btn-secondary" style={{ float: 'right' }}>
-							Next
-						</a>
-					</Link>
-				)}
+				<Pagination page={page} total={total} />
 			</Layout>
 		</div>
 	);
