ddfe0f8f6964c084bd95551f71c7979b73a41d8a
jinhlee19 <jinhlee19@gmail.com>
2022-07-02 03:52:38 +0900

    Authenticated Image Upload - 500 Err

--- components/ImageUpload.jsx
+++ components/ImageUpload.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { API_URL } from '@/config/index';
 import styles from '@/styles/Form.module.css';
 
-export default function ImageUpload({ evtId, imageUploaded }) {
+export default function ImageUpload({ evtId, imageUploaded, token }) {
 	const [image, setImage] = useState(null);
 	// console.log(image);
 	const handleSumbit = async e => {
@@ -16,6 +16,10 @@ export default function ImageUpload({ evtId, imageUploaded }) {
 
 		const res = await fetch(`${API_URL}/api/upload`, {
 			method: 'POST',
+			headers: {
+				// 'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`,
+			},
 			body: formData,
 		});
 		if (res.ok) {
